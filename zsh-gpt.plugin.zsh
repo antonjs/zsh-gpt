6f1076f9e165da7b0d37f7c27dbb4d63db2bd6a9
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2023-05-11 16:51:37 -0700

    add prompt, change g to gpt because g is reserved for git

--- zsh-gpt.plugin.zsh
+++ zsh-gpt.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-g() {
+gpt() {
   if [[ ! $+commands[curl] ]]; then echo "Curl must be installed."; return 1; fi
   if [[ ! $+commands[jq] ]]; then echo "Jq must be installed."; return 1; fi
   if [[ ! -v OPENAI_API_KEY ]]; then echo "Must set OPENAI_API_KEY to your API key"; return 1; fi
@@ -10,6 +10,6 @@ g() {
     -H "Authorization: Bearer $OPENAI_API_KEY" \
     -d '{
     "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "'"$*"'"}]
+    "messages": [{"role": "system", "content": "you''re an in-line zsh assistant running on linux. Your task is to answer the questions without any commentation at all, providing only the code to run on terminal. You can assume that the user understands that they need to fill in placeholders like <PORT>. Do not explain the answers, just give code. Keep the responses to one-liner answers as much as possible"}, {"role": "user", "content": "'"$*"'"}]
   }' | jq -r '.choices[0].message.content'
 }
